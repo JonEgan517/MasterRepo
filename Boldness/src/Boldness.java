59ee71a465f0a3674afb0c11e720c740bd830965
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2020-01-13 11:47:06 -0700

    commit

--- Boldness/src/Boldness.java
+++ Boldness/src/Boldness.java
@@ -1,17 +1,15 @@
-
 public class Boldness
 	{
-
 		public static void main(String[] args)
 			{
 				System.out.println("Be Bold");
+				
 				System.out.println("I want a pet monkey!");
 
 				System.out.println("No Be Bolder");
 
 				System.out.println("Hydrate or Diedrayte!");
-
+				
+				System.out.println("Hydrate you monkey!");
 			}
-		
-		
 	}
